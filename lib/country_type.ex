a1cf7d09565534b48b2db4c53915d7113b51dcd6
thousandsofthem <thousandsofthem@gmail.com>
2024-06-04 16:34:07 +0400

    add country prefix

--- lib/country_type.ex
+++ lib/country_type.ex
@@ -1,9 +1,10 @@
 defmodule Helper.Country do
   @enforce_keys [:iso, :name]
-  defstruct iso: nil, name: nil
+  defstruct iso: nil, name: nil, prefix: nil
 
   @type t :: %__MODULE__{
           iso: String.t(),
-          name: String.t()
+          name: String.t(),
+          prefix: String.t()
         }
 end
